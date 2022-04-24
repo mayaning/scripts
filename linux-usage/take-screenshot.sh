da224722ded8155e844f97843570d825191730a8
mayaning <mayaning4coding@163.com>
2022-04-24 14:37:34 +0800

    修改Linux下截图脚本

--- linux-usage/take-screenshot.sh
+++ linux-usage/take-screenshot.sh
@@ -1,10 +1,39 @@
 #!/bin/bash
+# #######################################################
+# Copyright 2021 - 2022 Ma Yaning
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# #######################################################
+#/
+##
+# @file take-screenshot.sh
+# @Brief    Linux下截图工具
+# @author Ma Yaning, <mayaning4coding@163.com>
+# @version 0.1
+# @date 2022-04-24
+#/
 
+
+# 截图的保存位置
 IMAGE_DIR=$HOME/图片/ScreenShot
+# 截图的名称
 IMAGE_NAME="$IMAGE_DIR/screenshot_$(date +%F_%H-%M-%S).png"
 
+# Step 1: 使用gnome-screenshot获取截图并保存
 gnome-screenshot -caf "$IMAGE_NAME"
 if [ -e "$IMAGE_NAME" ]
 then 
+    # Step 2: 将截图后的图片复制到粘贴板
+    xclip -selection clipboard -t image/png "$IMAGE_NAME"
+    # Step 3: 使用drawing打开截图，进行编辑(添加文件，箭头等)
     drawing "$IMAGE_NAME"
 fi

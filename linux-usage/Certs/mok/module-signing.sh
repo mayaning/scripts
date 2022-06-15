6ba2b4e4796a7e7a2026b2a4609cb1325c3577be
mayaning <mayaning4coding@163.com>
2022-06-15 20:19:36 +0800

    修改自签脚本

--- linux-usage/Certs/mok/module-signing.sh
+++ linux-usage/Certs/mok/module-signing.sh
@@ -41,14 +41,21 @@ function signing()
     export KBUILD_SIGN_PIN
 
     echo "sign-file"
-    $SIGNFILE "${hash_algo}" "${key}" "${x509}" \
-        "/lib/modules/$(uname -r)/misc/vboxdrv.ko"
+    #$SIGNFILE "${hash_algo}" "${key}" "${x509}" \
+    #    "/lib/modules/$(uname -r)/misc/vboxdrv.ko"
 
-    $SIGNFILE "${hash_algo}" "${key}" \
-        "${x509}" "/lib/modules/$(uname -r)/misc/vboxnetflt.ko"
+    #$SIGNFILE "${hash_algo}" "${key}" \
+    #    "${x509}" "/lib/modules/$(uname -r)/misc/vboxnetflt.ko"
+
+    #$SIGNFILE "${hash_algo}" "${key}" "${x509}" \
+    #    "/lib/modules/$(uname -r)/misc/vboxnetadp.ko"
 
     $SIGNFILE "${hash_algo}" "${key}" "${x509}" \
-        "/lib/modules/$(uname -r)/misc/vboxnetadp.ko"
+        "/lib/modules/$(uname -r)/misc/vmmon.ko"
+
+    $SIGNFILE "${hash_algo}" "${key}" "${x509}" \
+        "/lib/modules/$(uname -r)/misc/vmnet.ko"
+
 }
 
 # start from here

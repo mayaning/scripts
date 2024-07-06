f1f57819e104ba99e881ad4767518eaf53ff2612
mayaning <mayaning4coding@163.com>
2024-07-06 13:56:56 +0800

    add kde fictx5 config

--- linux-usage/setup-new-system/Fedora/02-enable-fcitx.sh
+++ linux-usage/setup-new-system/Fedora/02-enable-fcitx.sh
@@ -8,4 +8,6 @@ echo "export XMODIFIERS=\"@im=fcitx5\"" >> /etc/profile.d/fcitx.sh
 echo "export GLFW_IM_MODULE=ibus " >> /etc/profile.d/fcitx.sh
 chmod a+x /etc/profile.d/fcitx.sh
 
+#KDE: "虚拟键盘" 里选定fcitx
+
 
